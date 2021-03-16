94089902ab82308708cbbf3c0aa783844f44b81b
wangxiaoming8989 <wangxiaoming8989@sina.com>
2021-03-16 13:01:11 +0800

    fea: 重复的子字符串

---
+++ test/regexp/lesson1.test.js
@@ -0,0 +1,7 @@
+import subLoopStr from "../../code/regexp/lesson1";
+test("subLoopStr", () => {
+  expect(subLoopStr("abab")).toBe(true);
+});
+test("subLoopStr:2", () => {
+  expect(subLoopStr("abcabcabc")).toBe(true);
+});
