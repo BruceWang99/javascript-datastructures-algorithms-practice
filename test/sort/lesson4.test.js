c36fcadbb3f3c9c09e0942b05079bf290767d0e5
wangxiaoming8989 <wangxiaoming8989@sina.com>
2021-05-27 16:09:38 +0800

    fea: 缺失的第一个正数

---
+++ test/sort/lesson4.test.js
@@ -0,0 +1,10 @@
+import sort from "../../code/sort/lesson4.js";
+test("input:1", () => {
+  expect(sort([1, 2, 0])).toBe(3);
+});
+test("input:2", () => {
+  expect(sort([3, 4, -1, 1])).toBe(2);
+});
+test("input:3", () => {
+  expect(sort([7, 8, 9, 11, 12])).toBe(1);
+});
