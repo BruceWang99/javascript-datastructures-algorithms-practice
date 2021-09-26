5bbb9e9ad7d3541749704705f95faddc50d9967b
wangxiaoming8989 <wangxiaoming8989@sina.com>
2021-09-26 15:31:18 +0800

    feat: 括号匹配 栈方法

--- code/queue/isValid.js
+++ code/queue/isValid.js
@@ -8,13 +8,44 @@
  */
 var isValid = function(s) {
 // 思路1 正则 消消乐, 消除 () {} []这三种组合, 消除为空,一直循环消除, 直到消除为空字符串,就为true, 反之为false
-  while (s) {
-    if (s.match(/\(\)|\{\}|\[\]/g)) {
-      s = s.replace(/\(\)|\{\}|\[\]/g, '');
+//   while (s) {
+//     if (s.match(/\(\)|\{\}|\[\]/g)) {
+//       s = s.replace(/\(\)|\{\}|\[\]/g, '');
+//     } else {
+//       break;
+//     }
+//   }
+//   return !s;
+  // 思路2: 括号匹配是使用栈解决的经典问题。(后入先出)
+//   if (s.length % 2 === 1) return false; // s为奇数时返回false
+//   const sMap = {
+//     "{": '}',
+//     "(": ")",
+//     "[": "]"
+//   };
+//   const stack = [];
+//   for (const sub of s) {
+//     if (sMap[sub]) {
+//       stack.push(sub);
+//     } else {
+//       if (stack.length === 0 || sMap[stack.pop()] !== sub) return false; // 栈空或者出栈元素不等于当前元素时, false
+//     }
+//   }
+//   return stack.length === 0;
+  // 思路3: 思路2 节省空间复杂度
+  if (s.length % 2 === 1) return false; // s为奇数时返回false
+  const stack = [];
+  for (const sub of s) {
+    if (sub === "}") {
+      if (stack.pop() !== "{") return false; // 栈空或者出栈元素不等于当前元素时, false
+    } else if (sub === ")") {
+      if (stack.pop() !== "(") return false;
+    } else if (sub === "]") {
+      if (stack.pop() !== "[") return false;
     } else {
-      break;
+      stack.push(sub);
     }
   }
-  return !s;
+  return stack.length === 0;
 };
 export default isValid;
