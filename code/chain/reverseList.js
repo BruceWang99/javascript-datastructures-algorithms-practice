b8314b761aa873ed4707f8b75ef7cfb19f8652e4
wangxiaoming8989 <wangxiaoming8989@sina.com>
2021-09-26 11:26:33 +0800

    feat: 反转链表递归方法

--- code/chain/reverseList.js
+++ code/chain/reverseList.js
@@ -16,18 +16,26 @@ function ListNode(val, next) {
 }
 var reverseList = function(head) {
   // 思路1: 迭代, 反转后的链表可以从后一直往前接
-  let lastNode = null;
-  let cNode = null;
-  while (head) {
-    if (!lastNode) {
-      lastNode = new ListNode(head.val); // 最后一个节点没有next
-    } else {
-      cNode = new ListNode(head.val);
-      cNode.next = lastNode; // 当前节点next是上一个节点
-      lastNode = cNode; // 更换上一个节点
-    }
-    head = head.next;
+//   let lastNode = null;
+//   let cNode = null;
+//   while (head) {
+//     if (!lastNode) {
+//       lastNode = new ListNode(head.val); // 最后一个节点没有next
+//     } else {
+//       cNode = new ListNode(head.val);
+//       cNode.next = lastNode; // 当前节点next是上一个节点
+//       lastNode = cNode; // 更换上一个节点
+//     }
+//     head = head.next;
+//   }
+//   return lastNode;
+  // 递归, 边界: Node没有next, 子问题: 下一个节点的next = 当前节点 head.next.next = head
+  function reverse(head) {
+    if (!head || !head.next) return head;
+    let newHead = reverse(head.next);
+    newHead = newHead.next = head;
+    return head;
   }
-  return lastNode;
+  reverse(head);
 };
 export default reverseList;
